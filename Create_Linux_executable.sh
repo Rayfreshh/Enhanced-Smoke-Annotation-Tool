31bf423b4c39f9b71ff259cf1b2b61615923a478
Thomas-Dijksterhuis <dijksterhuis.t@gmail.com>
2025-07-28 14:40:26 +0200

    Created linux executable create

--- Create_Linux_executable.sh
+++ Create_Linux_executable.sh
@@ -2,11 +2,11 @@ python3.13 -m PyInstaller \
     --onefile \
     --hidden-import PIL._tkinter_finder \
     --noconsole \
-    --add-data "Smoke_application_image.ico;." \
+    --add-data "Smoke_application_image.ico:." \
     --icon=Smoke_application_image.ico \
-    --name="Smoke Annotation (windows)" \
+    --name="Smoke_Annotation_Linux" \
     --distpath=./ \
     Video_segment_editor.py
 
 rm -rf build
-rm "Smoke Annotation (windows).spec"
+rm "Smoke_Annotation_Linux.spec"
