2cbf99cc493aa58327669b0e6a76d3b78cc1ef05
Thomas-Dijksterhuis <dijksterhuis.t@gmail.com>
2025-07-28 16:10:47 +0200

    fixed wrong image filetype

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ if [[ "$OS_TYPE" == "Linux" ]]; then
   ICON=""
 elif [[ "$OS_TYPE" == MINGW* || "$OS_TYPE" == MSYS* || "$OS_TYPE" == CYGWIN* ]]; then
   NAME="Smoke Annotation (Windows)"
-  ICON="--icon=Smoke_application_image.ico"
+  ICON="--icon=Smoke_application_image.png"
 else
   echo "Unsupported OS: $OS_TYPE"
   exit 1
